3671685be76db7451bfdfc5f013f9e52e156a4dd
Ahmet CANDAN <candanahm@gmail.com>
2022-05-14 17:30:07 +0300

    Add project files.

---
+++ BlazorServerApp/Model/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAdminPanel.Model
+{
+    public enum PageOpeningType
+    {
+        New = 1,
+        Edit = 2,
+        View = 3,
+        Delete = 4,
+        List = 5
+    }
+}
